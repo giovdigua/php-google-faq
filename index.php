731c2858776873c0e50bdf2129469f7aabbb4642
giovdigua <giovdigua@gmail.com>
2020-01-21 18:56:22 +0100

    Setting Header

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
     $faqs = [
-        
+
     ]
 
 ?>
@@ -10,9 +10,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>php-google-faq</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+<header>
+    <nav>
+    <div id="logoSection">
+         <img src="img/googlelogo.png" alt="Logo Google">
+         <span> Privacy e termini</span>
+    </div>
+ 
+    <ul class="clearfix">
+        <li>Introduzione</li>
+        <li>Norme sulla privacy</li>
+        <li>Termini di servizio</li>
+        <li>Tecnologie</li>
+        <li class="active">Domande frequenti</li>
+        <li id="account">Account Google</li>
+    </ul>
+    </nav>
+</header>
+<main>
+
+</main>  
 </body>
 </html>
